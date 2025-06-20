288347804a02586f7456d6353b8b9a7145db4e36
IssamRP <aiissamsail@gmail.com>
2025-06-20 11:25:40 +0100

    Update LCS Length Algorithm.cpp

--- Dynamic Programming/Pattern - LCS/LCS Length Algorithm.cpp
+++ Dynamic Programming/Pattern - LCS/LCS Length Algorithm.cpp
@@ -101,6 +101,17 @@ int len_LCS(string &s1, string &s2, int n, int m) {
 	else return max(len_LCS(s1, s2, n, m - 1), 
 	                len_LCS(s1, s2, n - 1, m));
 }
+// dp version :
+ int longestCommonSubsequence(string t1, string t2) {
+        int n1=t1.size(),n2=t2.size();
+       vector<vector<int>>dp(n1+1,vector<int>(n2+1,0));
+        for(int i=n1-1;i>=0;i--){
+            for(int j=n2-1;j>=0;j--){
+                dp[i][j]=max(max(dp[i][j+1],dp[i+1][j]),(dp[i+1][j+1]+(t1[i]==t2[j])));
+            }
+        }
+        return dp[0][0];
+    }
 
 void solve()
 {
