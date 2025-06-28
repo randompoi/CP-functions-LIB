62a02062d2c3443ab68f3750a06746674cecca34
IssamRP <aiissamsail@gmail.com>
2025-06-28 12:05:25 +0100

    Create number of substrings divisible by M.cpp

---
+++ Number Theory/number of substrings divisible by M.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+// Counts how many substrings of s are divisible by M
+ll count_divisible_substrings(const string& s, int M) {
+    unordered_map<int, ll> count;
+    count[0] = 1;
+
+    int cur = 0;
+    int power = 1;
+    ll result = 0;
+
+    // Traverse string from right to left
+    for (int i = s.size() - 1; i >= 0; i--) {
+        int digit = s[i] - '0';
+        cur = (cur + digit * power) % M;
+        result += count[cur];
+        count[cur]++;
+        power = (power * 10) % M;
+    }
+
+    return result;
+}
